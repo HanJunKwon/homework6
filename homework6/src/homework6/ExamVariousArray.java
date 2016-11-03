65fdfa8038eb8414a157d016e81b634d6a9a7909
권한준 <skclaqks11@naver.com>
2016-11-03 20:52:21 +0900

    메인 함수 포함 클래스

--- homework6/src/homework6/ExamVariousArray.java
+++ homework6/src/homework6/ExamVariousArray.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class ExamVariousArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
+		
 		while(true){
-			System.out.println("1. 두번쨰로 큰 수 찾기");
+			System.out.println("1. 두번째로 큰 수 찾기");
 			System.out.println("2. 심사 점수 계산");
 			System.out.println("3. 학생 총점, 평균값 구하기");
 			System.out.println("4. 5층 아파트의 거주자 숫자 구하기");
