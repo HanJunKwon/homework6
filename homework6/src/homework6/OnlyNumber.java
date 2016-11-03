1febff23f760d0a0f3fafaa7c83f495826c4a00c
skclaqks11 <skclaqks11@naver.com>
2016-11-03 16:12:00 +0900

    중복된 값이 없는 배열

--- homework6/src/homework6/OnlyNumber.java
+++ homework6/src/homework6/OnlyNumber.java
@@ -1,5 +1,37 @@
 package homework6;
 
-public class OnlyNumber {
+import java.util.Scanner;
 
+public class OnlyNumber {
+	int[] number = new int[10];
+
+	public void InputNumber() {
+		int count = 0;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("1~100사이의 숫자를 입력하세요");
+
+		while (count<10) {
+			System.out.printf("%d번째 숫자 : ",count+1);
+			int n = sc.nextInt();
+			if (RedundancyCheck(count, n))
+				number[count++] = n;
+
+			else {
+				System.out.println("잘못 입력하였습니다. 다시 입력하세요");
+				continue;
+			}
+		}
+		
+		for(int i=9; i>=0; --i)
+			System.out.printf("%d번째 숫자는 %d입니다.\n", i+1, number[i]);
+	}
+
+	public boolean RedundancyCheck(int count, int n) {
+		for (int i = 0; i < count + 1; ++i) {
+			if (number[i] == n)
+				return false;
+		}
+		return true;
+	}
 }
