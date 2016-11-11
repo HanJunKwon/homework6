a9b733bc2cbb0dffa654d86c5aeb70474fc07dcd
skclaqks11 <skclaqks11@naver.com>
2016-11-11 12:44:37 +0900

    1~100까지 겹치지 않는 숫자 10개 받기(1~100까지 범위 지정 수정)

--- homework6/src/homework6/OnlyNumber.java
+++ homework6/src/homework6/OnlyNumber.java
@@ -4,28 +4,34 @@ import java.util.Scanner;
 
 public class OnlyNumber {
 	int[] number = new int[10];
-	int count=0;
-	
+	int count = 0;
+
 	public void InputNumber() {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("1~100사이의 숫자를 입력하세요");
 
-		while (count<10) {
-			System.out.printf("%d번째 숫자 : ",count+1);
+		while (count < 10) {
+			System.out.printf("%d번째 숫자 : ", count + 1);
 			int n = sc.nextInt();
-			if (RedundancyCheck(n))
-				number[count++] = n;
+
+			if (n < 1 || n > 100)
+				System.out.println("잘못 입력하였습니다. 다시 입력하세요.");
+			else {
+				if (RedundancyCheck(n))
+					number[count++] = n;
+			}
+
 		}
-		
+
 		System.out.println("============================");
-		for(int i=9; i>=0; --i)
-			System.out.printf("%d번째 숫자는 %d입니다.\n", i+1, number[i]);
+		for (int i = 9; i >= 0; --i)
+			System.out.printf("%d번째 숫자는 %d입니다.\n", i + 1, number[i]);
 	}
 
 	public boolean RedundancyCheck(int n) {
 		for (int i = 0; i < count + 1; ++i) {
-			if (number[i] == n || n<1 || n>100){
+			if (number[i] == n) {
 				System.out.println("잘못 입력하였습니다. 다시 입력하세요");
 				return false;
 			}
